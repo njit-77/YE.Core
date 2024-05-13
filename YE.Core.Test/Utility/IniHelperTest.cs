d86493afb239a4d779f86ecfecab2d7c15c6024a
njit_77 <512424964@qq.com>
2024-05-13 14:37:13 +0800

    add IniHelper

---
+++ YE.Core.Test/Utility/IniHelperTest.cs
@@ -0,0 +1,55 @@
+﻿using YE.Core.Utility;
+
+namespace YE.Core.Test.Utility
+{
+    public class IniHelperTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            string file_path = "./test.ini";
+
+            IniHelper.WriteValue("BOOL", "test_1", false, file_path);
+            IniHelper.WriteValue("BOOL", "test_2", true, file_path);
+
+            IniHelper.WriteValue("INT", "test_3", int.MinValue, file_path);
+            IniHelper.WriteValue("INT", "test_4", 0, file_path);
+            IniHelper.WriteValue("INT", "test_5", int.MaxValue, file_path);
+
+            IniHelper.WriteValue("FLOAT", "test_6", -1234.567, file_path);
+            IniHelper.WriteValue("FLOAT", "test_7", 0, file_path);
+            IniHelper.WriteValue("FLOAT", "test_8", 9876.543, file_path);
+
+            IniHelper.WriteValue("DOUBLE", "test_9", -123456789.987654, file_path);
+            IniHelper.WriteValue("DOUBLE", "test_10", 0, file_path);
+            IniHelper.WriteValue("DOUBLE", "test_11", 987654321.123456, file_path);
+
+            IniHelper.WriteValue("STRING", "test_12", "你好", file_path);
+            IniHelper.WriteValue("STRING", "test_13", "Hello", file_path);
+            IniHelper.WriteValue("STRING", "test_14", "안녕하세요", file_path);
+            IniHelper.WriteValue("STRING", "test_15", "お元気ですか", file_path);
+            IniHelper.WriteValue("STRING", "test_16", "¿Qué tal", file_path);
+
+            Assert.False(IniHelper.ReadValue<bool>("BOOL", "test_1", file_path));
+            Assert.True(IniHelper.ReadValue<bool>("BOOL", "test_2", file_path));
+
+            Assert.Equal(int.MinValue, IniHelper.ReadValue<int>("INT", "test_3", file_path));
+            Assert.Equal(0, IniHelper.ReadValue<int>("INT", "test_4", file_path));
+            Assert.Equal(int.MaxValue, IniHelper.ReadValue<int>("INT", "test_5", file_path));
+
+            Assert.Equal(-1234.567f, IniHelper.ReadValue<float>("FLOAT", "test_6", file_path));
+            Assert.Equal(0, IniHelper.ReadValue<float>("FLOAT", "test_7", file_path));
+            Assert.Equal(9876.543f, IniHelper.ReadValue<float>("FLOAT", "test_8", file_path));
+
+            Assert.Equal(-123456789.987654, IniHelper.ReadValue<double>("DOUBLE", "test_9", file_path));
+            Assert.Equal(0, IniHelper.ReadValue<double>("DOUBLE", "test_10", file_path));
+            Assert.Equal(987654321.123456, IniHelper.ReadValue<double>("DOUBLE", "test_11", file_path));
+
+            Assert.Equal("你好", IniHelper.ReadValue<string>("STRING", "test_12", file_path));
+            Assert.Equal("Hello", IniHelper.ReadValue<string>("STRING", "test_13", file_path));
+            Assert.Equal("안녕하세요", IniHelper.ReadValue<string>("STRING", "test_14", file_path));
+            Assert.Equal("お元気ですか", IniHelper.ReadValue<string>("STRING", "test_15", file_path));
+            Assert.Equal("¿Qué tal", IniHelper.ReadValue<string>("STRING", "test_16", file_path));
+        }
+    }
+}
---
+++ YE.Core/Utility/IniHelper.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace YE.Core.Utility
+{
+    /// <summary>
+    /// ini文件操作类
+    /// 1、支持bool、int、float、double、string类型;
+    /// 2、float类型有效位数7位;double类型有效位数15位;
+    /// 3、string类型有效位数512位;
+    /// 4、当string类型为多语言且出现乱码时，需提前创建ini文件且格式为UTF16 LE;
+    /// </summary>
+    public class IniHelper
+    {
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        /// <summary>
+        /// 写入数据
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="section">节</param>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        /// <param name="filePath">ini文件</param>
+        /// <returns>是否写入成功</returns>
+        public static bool WriteValue<T>(string section, string key, T value, string filePath)
+        {
+            return WritePrivateProfileString(section, key, value.ToString(), filePath) != 0;
+        }
+
+        /// <summary>
+        /// 读取数据
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="section">节</param>
+        /// <param name="key">键</param>
+        /// <param name="filePath">ini文件</param>
+        /// <param name="def">默认值</param>
+        /// <returns></returns>
+        public static T ReadValue<T>(string section, string key, string filePath, T def = default(T))
+        {
+            int count = 512;
+
+            var sb = new StringBuilder(count);
+            GetPrivateProfileString(section, key, "", sb, count, filePath);
+
+            if (typeof(T) == typeof(bool))
+            {
+                if (bool.TryParse(sb.ToString(), out bool result))
+                {
+                    return (T)(object)result;
+                }
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                if (int.TryParse(sb.ToString(), out int result))
+                {
+                    return (T)(object)result;
+                }
+            }
+            else if (typeof(T) == typeof(float))
+            {
+                if (float.TryParse(sb.ToString(), out float result))
+                {
+                    return (T)(object)result;
+                }
+            }
+            else if (typeof(T) == typeof(double))
+            {
+                if (double.TryParse(sb.ToString(), out double result))
+                {
+                    return (T)(object)result;
+                }
+            }
+            else if (typeof(T) == typeof(string))
+            {
+                return (T)(object)sb.ToString();
+            }
+
+            return def;
+        }
+    }
+}
