7c27eaeed7b660ea9bc30771010cd28fdae430da
njit_77 <512424964@qq.com>
2024-05-28 11:50:05 +0800

    add nuget Description

--- YE.Core.Test/Algorithms/CrcHelperTest.cs
+++ YE.Core.Test/Algorithms/CrcHelperTest.cs
@@ -10,9 +10,9 @@ namespace YE.Core.Test.Algorithms
         {
             byte[] data = Encoding.Unicode.GetBytes("今天天气很好");
 
-            Assert.Equal(CrcHelper.GetCrc16(data, 0x1021, false), (UInt16)0x5DC1);
-            Assert.Equal(CrcHelper.GetCrc16(data, 0x8005, true), (UInt16)0xD63B);
-            Assert.Equal(CrcHelper.GetCrc32(data, 0x04C11DB7, true), (UInt32)0x7B1B0DAA);
+            Assert.Equal((UInt16)0x5DC1, CrcHelper.GetCrc16(data, 0x1021, false));
+            Assert.Equal((UInt16)0xD63B, CrcHelper.GetCrc16(data, 0x8005, true));
+            Assert.Equal((UInt32)0x7B1B0DAA, CrcHelper.GetCrc32(data, 0x04C11DB7, true));
         }
     }
 }
